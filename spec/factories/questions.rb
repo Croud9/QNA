5d41ee571efec1afd9cce43adce53cf2aabad7c3
Luvest <slavokugfk@gmail.com>
2021-04-08 22:48:28 +0300

    User can attach files while editing, delete file from answer, question, direct upload on Amazon S3 with specs.

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,11 +1,17 @@
+include ActionDispatch::TestProcess
+
 FactoryBot.define do
   factory :question do
     title { "Question String" }
     body { "Question Text" }
     user
-    
+
     trait :invalid do
       title { nil }
+    end
+
+    trait :with_file do
+      files { fixture_file_upload("#{Rails.root}/spec/rails_helper.rb") }
     end
   end
 end
