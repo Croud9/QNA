8f4183ab7fcbf70b43942b5bbc033e9d49620844
Luvest <slavokugfk@gmail.com>
2021-04-12 15:46:30 +0300

    User can add links when creating answer, question with spec

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,10 +9,12 @@ class QuestionsController < ApplicationController
 
   def show
     @answer = Answer.new
+    @answer.links.new
   end
 
   def new
     @question = Question.new
+    @question.links.new
   end
 
   def edit
@@ -44,7 +46,8 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body, files: [])
+    params.require(:question).permit(:title, :body,
+                                      files: [], links_attributes: [:name, :url])
   end
 
   def check_author
---
+++ spec/features/answer/add_link_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'User can add links to answer' do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:gist_url) { 'https://gist.github.com/Croud9/82df53ec62c8624015258e376d24b193' }
+  given(:google_url) { 'https://google.com' }
+
+  scenario 'User adds link when asks answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    fill_in 'Body', with: 'Answer text'
+
+    fill_in 'Link name', with: 'My gist'
+    fill_in 'Url', with: gist_url
+
+    click_on 'Add link'
+
+    within all('.new-link').last do
+      fill_in 'Link name', with: 'Google'
+      fill_in 'Url', with: google_url
+    end
+
+    click_on 'Answer'
+
+    within '.answers' do
+      expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Google', href: google_url
+    end
+  end
+end
