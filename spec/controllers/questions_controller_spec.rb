a29cbe5fc47e77366bfe44f8eb04cd0fefe7304c
Luvest <slavokugfk@gmail.com>
2021-04-12 19:58:15 +0300

    Fix

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -86,7 +86,6 @@ RSpec.describe QuestionsController, type: :controller do
       end
     end
 
-
     context 'for unauthenticated user' do
       before { get :edit, params: { id: question } }
 
@@ -120,7 +119,7 @@ RSpec.describe QuestionsController, type: :controller do
       it 'does not save the question' do
         expect { post :create, format: :js,
                  params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
-
+      end
 
       it 're-renders new view' do
         post :create, params: { question: attributes_for(:question, :invalid) }, format: :js
@@ -184,79 +183,79 @@ RSpec.describe QuestionsController, type: :controller do
     end
 
     context 'for not the author of the question' do
-     let(:not_author) { create(:user) }
+      let(:not_author) { create(:user) }
 
-     before { login(not_author) }
-     before { patch :update, params: { id: question, question: {  title: 'title', body: 'body' } } }
+      before { login(not_author) }
+      before { patch :update, params: { id: question, question: {  title: 'title', body: 'body' } } }
 
-     it 'does not change answer' do
-       question.reload
+      it 'does not change answer' do
+        question.reload
 
-       expect(question.title).to eq 'Question String'
-       expect(question.body).to eq 'Question Text'
-     end
+        expect(question.title).to eq 'Question String'
+        expect(question.body).to eq 'Question Text'
+      end
 
-     it 're-renders edit view' do
-       expect(response).to redirect_to question
-     end
-   end
+      it 're-renders edit view' do
+        expect(response).to redirect_to question
+      end
+    end
 
-   context 'for unauthenticated user' do
-     before { patch :update, params: { id: question, question: {  title: 'title', body: 'body' } } }
+    context 'for unauthenticated user' do
+      before { patch :update, params: { id: question, question: {  title: 'title', body: 'body' } } }
 
-     it 'does not change question' do
-       question.reload
+      it 'does not change question' do
+        question.reload
 
-       expect(question.title).to eq 'Question String'
-       expect(question.body).to eq 'Question Text'
-     end
+        expect(question.title).to eq 'Question String'
+        expect(question.body).to eq 'Question Text'
+      end
 
-     it 'redirects to sign up page' do
-       expect(response).to redirect_to new_user_session_path
-     end
-   end
- end
-
- describe 'DELETE #destroy' do
-   let!(:question) { create(:question) }
-
-   context 'for the author of the question' do
-     before { login(user) }
-
-     it 'deletes the question' do
-       expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
-     end
-
-    it 'redirects to index' do
-      delete :destroy, params: { id: question }
-      expect(response).to redirect_to questions_path
+      it 'redirects to sign up page' do
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 
-  context 'for not the author of the answer' do
-    let(:not_author) { create(:user) }
+  describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
 
-    before { login(not_author) }
+    context 'for the author of the question' do
+      before { login(user) }
 
-    it "don't delete the question" do
-      expect { delete :destroy, params: { id: question } }.to_not change(Question, :count)
+      it 'deletes the question' do
+        expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+      end
+
+      it 'redirects to index' do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to questions_path
+      end
     end
 
-    it 'redirects to question' do
-      delete :destroy, params: { id: question }
-      expect(response).to redirect_to question_path(question)
-    end
-  end
+    context 'for not the author of the answer' do
+      let(:not_author) { create(:user) }
 
-  context 'for unauthenticated user' do
-    it "don't delete the question" do
-      expect { delete :destroy, params: { id: question } }.to_not change(Question, :count)
+      before { login(not_author) }
+
+      it "don't delete the question" do
+        expect { delete :destroy, params: { id: question } }.to_not change(Question, :count)
+      end
+
+      it 'redirects to question' do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to question_path(question)
+      end
     end
 
-    it 'redirects to sign up page' do
-      delete :destroy, params: { id: question }
-      expect(response).to redirect_to new_user_session_path
+    context 'for unauthenticated user' do
+      it "don't delete the question" do
+        expect { delete :destroy, params: { id: question } }.to_not change(Question, :count)
+      end
+
+      it 'redirects to sign up page' do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to new_user_session_path
+      end
     end
-   end
   end
 end
